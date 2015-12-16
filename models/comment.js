c19a388c114c1e2b50a443211f4b8c77375f063c
sarjak <gandhi.sarjak@gmail.com>
2015-12-16 11:26:42 +0530

    Updated comments Router and model

--- models/comment.js
+++ models/comment.js
@@ -13,10 +13,10 @@ var Schema = mongoose.Schema;
 var commentSchema = new Schema({
 
 	// id of the user
-	user_id: {type:Schema.Types.ObjectId,ref:"UserSchema", required:true, unique:false},
+	user_id: {type:Schema.Types.ObjectId, ref:"UserSchema", required:true, unique:false},
 
 	// id of the post
-	post_id: {type:Schema.Types.ObjectId,ref:"PostSchema", required:true, unique:false},
+	post_id: {type:Schema.Types.ObjectId, ref:"PostSchema", required:true, unique:false},
 
 	// date of the comment
 	comment_date: {type:Date, required:true,  default:Date.now() },
--- routes/comments.js
+++ routes/comments.js
@@ -1,13 +1,37 @@
+// File: comments.js
+// Description: Controller of Comment Model
+// Author: Shubham Agrawal
+// Edited By: Sarjak Gandhi
+
 var express = require('express');
 var router = express.Router();
 var commentSchema = require('../models/comment');
 var nodemailer = require('nodemailer');
 
+
+// Controller: List all comments in the database
+// URL: http://www.domain.com/comments
+// Method: GET
+// Paramaters: NO Paramters
+// Returns: Object containing list of all the comments in the database
+
+router.get('/',function(req,res,next){
+	commentSchema.find({}, function(err, comment){
+		if(err){
+			console.log(err);
+			res.json(err);
+		}
+		else{
+			res.json(comment);
+		}
+	});
+});
+
 // Controller: List all comment of particular post
-// URL: http://www.domain.com/post/getcomment/:postid
+// URL: http://www.domain.com/comments/getcomment/:postid
 // Method: GET
 // Paramaters: post_id
-// Returns: Object containing list of all the comment in the database
+// Returns: Object containing list of all the comment on given postid
 
 router.get('/getcomment/:post_id',function(req,res,next){
 	commentSchema.find({ post_id : req.params.post_id }, function(err, comment){
@@ -22,13 +46,12 @@ router.get('/getcomment/:post_id',function(req,res,next){
 });
 
 // Controller: Return Specific comment
-// URL: http://www.domain.com/post/comment/:commentid
+// URL: http://www.domain.com/comments/:commentid
 // Mehod: GET
 // Parameters: comment_id
-// Returns: comment object if found in the database, else error.
+// Returns: Comment object if found in the database, else error.
 
-router.get('/post/comment/:comment_id', function(req,res,next){
-	
+router.get('/:comment_id', function(req,res,next){
 	commentSchema.find({ _id : req.params.comment_id }, function(err,comment){
 		if(err){
 			console.log(err);
@@ -41,13 +64,13 @@ router.get('/post/comment/:comment_id', function(req,res,next){
 });
 
 
-// Controller: insert comment
-// URL: http://www.domain.com/post/comment
+// Controller: Add comment
+// URL: http://www.domain.com/comments/add
 // Mehod: POST
-// Parameters:comment details
+// Parameters: comment details
 // Returs: status
 
-router.post('/post/comment', function(req,res,next) {
+router.post('/add', function(req,res,next) {
 	
 	var response_object;
 	// Create new user Object
@@ -59,24 +82,24 @@ router.post('/post/comment', function(req,res,next) {
 	if(err) {
 		res.json(err);
 	} else {
-		comment.save(function(err, user, numAffected) {
+		comment.save(function(err, cmnt, numAffected) {
 			if(err) {
 				console.log(err);
 				res.json(err);
 			} else {
-					console.log("success");
+				res.json(cmnt);
 			}
 		});	
 	}
 });
 
 // Controller: Delete comment
-// URL: http://www.domain.com/post/deletecomment/<commentid>
+// URL: http://www.domain.com/comments/delete/<commentid>
 // Mehod: DELETE
 // Parameters: commentid
 // Returns: "Successfully Deleted" message, else error message.
 
-router.delete('/post/deletecomment/:comment_id',function(req,res,next){
+router.delete('/delete/:comment_id',function(req,res,next){
 
 	var response_obj;
 	commentSchema.remove({_id:req.params.comment_id},function(err,result){
@@ -84,37 +107,37 @@ router.delete('/post/deletecomment/:comment_id',function(req,res,next){
 			console.log(err);
 			res.json(err);
 		}else{
-			response_obj = { status : 1 , message : "comment Deleted"};
+			response_obj = { status : 1 , message : "Comment Deleted"};
 			res.json(response_obj);
 		}
 	});	
 });
 
-// Controller: Update a user information
-// URL: http://www.domain.com/post/editcomment/<commentid>
+// Controller: Update a comment
+// URL: http://www.domain.com/comments/edit/<commentid>
 // Method: POST
 // Parameters: Attributes of comment.
 // Returns: Status and Message
 
-router.post('/post/editcomment/:comment_id', function(req, res, next){
+router.post('/edit/:comment_id', function(req, res, next){
 	var response_obj;
 
 	var query = commentSchema.where( {_id : req.params.comment_id} );
 
 	query.findOneAndUpdate( { "$set" : req.body },
-		function(err, coupon){
+		function(err, cmnt){
 			if(err){
 				console.log(err);
 					response_obj={status:1, message:"Something went wrong"};
 					res.send(response_obj);
 			}
 			else{
-				if(coupon){
-					response_obj={status:2, message:"comment Updated"};
+				if(cmnt){
+					response_obj={status:2, message:"Comment Updated"};
 					res.send(response_obj);
 				}
 				else{
-					response_obj={status:3, message:"comment not found"};
+					response_obj={status:3, message:"Comment Not Found"};
 					res.send(response_obj);
 				}
 
