1f9253a8855f8566492a89cb58628efb73c26377
sarjak <gandhi.sarjak@gmail.com>
2015-12-16 11:38:36 +0530

    Updated feedbacks.js Router

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -1,16 +1,20 @@
+// File: feedbacks.js
+// Description: Controller of Comment Feedback
+// Author: Shubham Agrawal
+// Edited By: Sarjak Gandhi
+
 var express = require('express');
 var router = express.Router();
 var userSchema = require('../models/user');
 var feedbackSchema = require('../models/feedback');
-var nodemailer = require('nodemailer');
 
 // Controller: List all Feedback
-// URL: http://www.domain.com/feedback/
+// URL: http://www.domain.com/feedbacks
 // Method: GET
 // Paramaters: No parameters
-// Returns: Object containing list of all the feedback in the database
+// Returns: Object containing list of all the feedbacks in the database
 
-router.get('/feedback',function(req,res,next){
+router.get('/',function(req,res,next){
 	feedbackSchema.find({}, function(err, feedback){
 		if(err){
 			console.log(err);
@@ -22,13 +26,13 @@ router.get('/feedback',function(req,res,next){
 	});
 });
 
-// Controller: insert feedback
-// URL: http://www.domain.com/user/givefeedback
+// Controller: Add feedback
+// URL: http://www.domain.com/feedbacks/add
 // Mehod: POST
 // Parameters: feedback details
-// Returns: staus
+// Returns: Object of Feedback posted.
 
-router.post('/user/givefeedback', function(req,res,next) {
+router.post('/add', function(req,res,next) {
 	
 	var response_object;
 	// Create new feedback Object
@@ -46,10 +50,31 @@ router.post('/user/givefeedback', function(req,res,next) {
 				console.log(err);
 				res.json(err);
 			} else {
-				console.log("success");
+				res.json(feedback);
 			}
 		});	
 	}
 });
+
+// Controller: Delete Feedback
+// URL: http://www.domain.com/feedabacks/delete/<feedbackid>
+// Mehod: DELETE
+// Parameters: feedbackid
+// Returns: "Successfully Deleted" message, else error message.
+
+router.delete('/delete/:feedback_id',function(req,res,next){
+
+	var response_obj;
+	feedbackSchema.remove({_id:req.params.feedback_id},function(err,result){
+		if(err){
+			console.log(err);
+			res.json(err);
+		}else{
+			response_obj = { status : 1 , message : "Feedback Deleted"};
+			res.json(response_obj);
+		}
+	});	
+});
+
 
 module.exports = router;
